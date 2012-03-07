4e7342b1905cfb2278560bafb8ea07673973501c
Chris Ortman <chriso@innovsys.com>
2012-03-07 11:06:44 -0600

    Add example of combinatorial testing

---
+++ MsTestProject/5_CombinatorialTesting.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using ApprovalTests.Combinations;
+using ApprovalTests.Reporters;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MsTestProject.Lib;
+using System.Linq;
+
+namespace MsTestProject
+{
+    [TestClass]
+    [UseReporter(typeof(DiffReporter))]
+    public class CombinatorialTesting
+    {
+        
+        [TestMethod]
+        public void VerifyCustomerPaymentOptions()
+        {
+            var possiblePaymentTypes = new[]
+            {
+                "BankDraft, CreditCard, Cash",
+                "BankDraft, CreditCard",
+                "BankDraft, Cash",
+                "CreditCard, Cash",
+                "CreditCard",
+                "Cash",
+                "BankDraft",
+            };
+            var amounts = new[] {0M, 10M, 100M, 200M};
+            var customerLocations = new[] {"SD", "IA"};
+
+            CombinationApprovals.VerifyAllCombinations((allowedPaymentTypes,amount,location) =>
+            {
+                var paymentService = new PaymentService();
+                var parts = allowedPaymentTypes.Split(',');
+
+                paymentService.ApplicationAllowedPaymentTypes =
+                    parts.Select(x => (PaymentTypes) System.Enum.Parse(typeof(PaymentTypes), x)).ToArray();
+
+                var customer = new Customer()
+                {
+                    HomeAddress = new Address() {State = location}
+                };
+
+                var customerAllowedPaymentTypes = paymentService.GetAllowedPaymentTypes(customer, amount).ToArray();
+                return string.Join(",", customerAllowedPaymentTypes);
+            },possiblePaymentTypes,amounts,customerLocations);
+        }
+    }
+}
---
+++ MsTestProject/Lib/PaymentService.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace MsTestProject.Lib
+{
+    public enum PaymentTypes
+    {
+        CreditCard,
+        BankDraft,
+        Cash
+    }
+
+    public class SystemLevelOptions
+    {
+        public bool AreCreditCardMerchantsSetup(Customer c)
+        {
+            //maybe have some system level tables
+
+            //then need to check for the customer
+            //in this example we've only configured for 
+            //customers in SD or NE state
+            if(c.HomeAddress != null && 
+                c.HomeAddress.State == "SD")
+            {
+                return true;
+            }
+            else if(c.HomeAddress != null &&
+                c.HomeAddress.State == "NE")
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+    public class PaymentService
+    {
+
+        public PaymentTypes[] ApplicationAllowedPaymentTypes { get; set; }
+
+        public IEnumerable<PaymentTypes> GetAllowedPaymentTypes(Customer c,decimal amount)
+        {
+            var systemOptions = new SystemLevelOptions();
+            
+            //we'll allways allow cash
+            yield return PaymentTypes.Cash;
+            
+            //only allow bank if amount less than $100 and it is in our allowed payment types
+            if(ApplicationAllowedPaymentTypes.Contains(PaymentTypes.BankDraft))
+            {
+                if(amount < 100M)
+                {
+                    yield return PaymentTypes.BankDraft;
+                }
+            }
+
+            if(systemOptions.AreCreditCardMerchantsSetup(c))
+            {
+                if(ApplicationAllowedPaymentTypes.Contains(PaymentTypes.CreditCard))
+                {
+                    yield return PaymentTypes.CreditCard;
+                }
+            }
+        }
+    }
+}
